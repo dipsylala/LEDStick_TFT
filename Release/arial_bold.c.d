555a32168f771fb8cc426b405f1063e4a908111e
marcu <marcu@DEADPOOL>
2017-02-19 19:31:40 +0000

    Tidy-up to remove unwanted binaries

--- Release/arial_bold.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\marcu\AppData\Local\Temp\VMBuilds\LEDStick_TFT\mega_atmega2560\Release\arial_bold.c.o: \
- C:\Users\marcu\AppData\Local\Temp\VMBuilds\LEDStick_TFT\mega_atmega2560\Release\arial_bold.c
